3a441a1e0cdd0e9cef0de6556c3f5014804bedad
Daisuke Kusatake <dosaisen@gmail.com>
2022-09-12 00:13:45 +0900

    fix

--- raw/designMode.js
+++ raw/designMode.js
@@ -1,18 +1,51 @@
 javascript:(
   function(){
     function designModeOn() {
+      // 触れなくさせるスタイルを適用させる
       const styleEl = document.createElement('style')
-      styleEl.setAttribute("id", `${location.pathname}`)
-      document.head.appendChild(styleEl)
-      styleEl.textContent = 'html * {pointer-events: none !important;}'
+      styleEl.setAttribute("id", `${location.pathname}__style`)
+      document.head.prepend(styleEl)
+      styleEl.textContent = `
+        html *:not(div#${location.pathname}__alert) {
+          pointer-events: none !important;
+        }
+        div#${CSS.escape(location.pathname)}__alert {
+          position: fixed;
+          top: 0;
+          right: 0;
+          background-color: white;
+          border: solid black thin;
+          z-index: 99999;
+          cursor: pointer;
+        }
+      `
+      
+      // 編集解除要素
+      const alertEl = document.createElement('div')
+      alertEl.setAttribute("id", `${location.pathname}__alert`)
+      document.body.prepend(alertEl)
+      alertEl.textContent = '編集解除'
+
+      // 『編集解除』をクリックしたら編集解除する
+      alertEl.addEventListener('click', designModeOff)
+
+      // デザインモードをONにする
       document.designMode = "on"
     }
-    const styleEl = document.querySelector(`style#${CSS.escape(location.pathname)}`)
+
+    function designModeOff() {
+      const styleEl = document.querySelector(`style#${CSS.escape(location.pathname)}__style`)
+      const alertEl = document.querySelector(`div#${CSS.escape(location.pathname)}__alert`)
+      styleEl.remove()
+      alertEl.remove()
+      document.designMode = "off"
+    }
+
+    const styleEl = document.querySelector(`style#${CSS.escape(location.pathname)}__style`)
     if (styleEl === null) {
       designModeOn()
     } else {
-      styleEl.remove()
-      document.designMode = "off"
+      designModeOff()
     }
   }
 )()
